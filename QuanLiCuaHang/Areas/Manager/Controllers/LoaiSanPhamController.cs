92a375f782ea77f3a9c9a7d5ba7354db589947bb
BaLinh <balinh@gmail.com>
2020-06-05 15:15:46 +0700

    Project commit

---
+++ QuanLiCuaHang/Areas/Manager/Controllers/LoaiSanPhamController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using QuanLiCuaHang.Models;
+
+namespace QuanLiCuaHang.Areas.Manager.Controllers
+{
+ 
+    public class LoaiSanPhamController : Controller
+    {
+        // GET: Manager/LoaiSanPham
+
+        public ActionResult ChiTiet(int id)
+        {
+            LoaiSanPhamContext loaisanphamContext = new LoaiSanPhamContext();
+            LoaiSanPham loaiSanPham = loaisanphamContext.LoaiSanPham.Single(lsp => lsp.MaLoaiSP == id);
+            //LoaiSanPham loaiSanPham = loaisanphamContext.LoaiSanPham.Single(lsp => lsp.MaLoaiSanPham == MaLoaiSanPham);
+            return View(loaiSanPham);
+        }
+
+        public ActionResult Test()
+        {
+            LoaiSanPhamContext loaisanphamContext = new LoaiSanPhamContext();
+            List<LoaiSanPham> loaiSanPham = loaisanphamContext.LoaiSanPham.ToList();
+            //LoaiSanPham loaiSanPham = loaisanphamContext.LoaiSanPham.Single(lsp => lsp.MaLoaiSanPham == MaLoaiSanPham);
+            return View(loaiSanPham);
+        }
+    }
+}
---
+++ QuanLiCuaHang/Models/LoaiSanPhamContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace QuanLiCuaHang.Models
+{
+    public class LoaiSanPhamContext : DbContext 
+    {
+        public DbSet<LoaiSanPham> LoaiSanPham { get; set; }
+    }
+}
